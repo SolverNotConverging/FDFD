772a038ce339a8e4a75f83fda5dd500fbe929849
91MrShen <mostima0@outlook.com>
2025-08-29 13:19:43 +0800

    Update the PML function.

--- Mode_Solver_2D/2D_Field_Visualization.py
+++ Mode_Solver_2D/2D_Field_Visualization.py
@@ -1,19 +1,19 @@
 from FDFD_Mode_Solver import FDFDModeSolver
 
 # Parameters for the simulation
-x_range = 40e-3  # 40 mm in x-direction
+x_range = 30e-3  # 30 mm in x-direction
 y_range = 10e-3  # 10 mm in y-direction
-Nx = 400  # Grid points in x
+Nx = 300  # Grid points in x
 Ny = 100  # Grid points in y
 frequency = 25e9  # 25 GHz
 num_modes = 15  # Number of modes to compute
 
 # Initialize solver and define structure
 solver = FDFDModeSolver(frequency, x_range, y_range, Nx, Ny, num_modes)
-solver.add_object(-1e8, 1, (0, 150), (44, 45))
-solver.add_object(-1e8, 1, (250, 400), (44, 45))
-solver.add_object(10.2, 1, (0, 400), (45, 58))
-solver.add_object(-1e8, 1, (0, 400), (58, 59))
+solver.add_object(-1e8, 1, (0, 100), (44, 45))
+solver.add_object(-1e8, 1, (200, 300), (44, 45))
+solver.add_object(10.2, 1, (0, 300), (45, 58))
+solver.add_object(-1e8, 1, (0, 300), (58, 59))
 solver.add_UPML(25, 3, 5, direction='x')
 
 # Solve for the eigenmodes
