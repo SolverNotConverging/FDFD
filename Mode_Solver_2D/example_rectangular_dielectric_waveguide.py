632a3196484917c30fa458ab44416d58d29daf20
Life is Hard <mostima0@outlook.com>
2026-02-24 06:13:11 +0000

    improve some examples

--- Mode_Solver_2D/example_rectangular_dielectric_waveguide.py
+++ Mode_Solver_2D/example_rectangular_dielectric_waveguide.py
@@ -1,11 +1,11 @@
 from FDFD_Mode_Solver import FDFDModeSolver
 
 # Rectangular waveguide cross section
-x_range = 50e-3
-y_range = 50e-3
+x_range = 20e-3
+y_range = 20e-3
 Nx = 200
 Ny = 200
-frequency = 50e9
+frequency = 30e9
 num_modes = 6
 
 solver = FDFDModeSolver(frequency, x_range, y_range, Nx, Ny, num_modes)
---
+++ Mode_Solver_2D/example_ridge_dielectric_waveguide.py
@@ -0,0 +1,21 @@
+from FDFD_Mode_Solver import FDFDModeSolver
+
+# Ridge waveguide cross-section
+x_range = 24e-3
+y_range = 16e-3
+Nx = 240
+Ny = 160
+frequency = 30e9
+num_modes = 6
+
+solver = FDFDModeSolver(frequency, x_range, y_range, Nx, Ny, num_modes)
+
+# Slab
+solver.add_object(6.0, 1, (0, Nx), (60, 80))
+
+# Ridge
+solver.add_object(12.0, 1, (100, 140), (80, 100))
+
+solver.add_UPML(pml_width=20, n=3, sigma_max=4, direction="x")
+solver.solve()
+solver.visualize_with_gui()
