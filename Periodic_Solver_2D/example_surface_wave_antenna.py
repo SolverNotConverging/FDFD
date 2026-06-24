6527b08fa898ea8f22f346490bde1b0acb048eea
Traveler <mostima0@outlook.com>
2026-06-24 09:47:50 +0800

    change some examples

--- Periodic_Solver_2D/example_surface_wave_antenna.py
+++ Periodic_Solver_2D/example_surface_wave_antenna.py
@@ -5,7 +5,7 @@ x_range = 10e-3
 z_range = 8e-3
 Nx = 200
 Nz = 80
-frequency = 25e9
+frequency = 20e9
 num_modes = 8
 guess = 5
 
@@ -13,13 +13,12 @@ solver = PeriodicModeSolver2D("TM", freq=frequency, x_range=x_range, z_range=z_r
                               num_modes=num_modes, guess=guess, mode_filter=False)
 
 # Ground/metal edges
-solver.add_pec((25, 26), (0, 10))
-solver.add_pec((9, 10), (0, Nz))
+solver.add_pec((2.27e-3, 2.37e-3), (1.0e-3, 2.0e-3))
+solver.add_pec((0.9e-3, 1.0e-3), (0, Nz))
 
 # Dielectric loading
-solver.add_rectangle(10.2, 1, (10, 23), (0, Nz))
-solver.add_rectangle(7, 1, (23, 25), (0, Nz))
+solver.add_rectangle(10.2, 1, (1.0e-3, 2.27e-3), (0, Nz), subpixels=8)
 
-solver.add_pml(pml_width=50, n=3, sigma_max=5, direction="x+")
+solver.add_pml(pml_width=30, n=3, sigma_max=5, direction="x+")
 solver.solve()
 solver.visualize_with_gui()
